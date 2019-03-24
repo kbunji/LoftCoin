18f468bdc4600c91a89d2e15d0a645cbefea1770
kztyanrv <tyanrv@gmail.com>
2019-03-24 13:14:31 +0600

    import ButterKnife, update WelcomeActivity

--- app/src/main/java/com/tyanrv/loftcoin/screens/welcome/WelcomeActivity.java
+++ app/src/main/java/com/tyanrv/loftcoin/screens/welcome/WelcomeActivity.java
@@ -3,10 +3,17 @@ package com.tyanrv.loftcoin.screens.welcome;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.tyanrv.loftcoin.App;
 import com.tyanrv.loftcoin.R;
+import com.tyanrv.loftcoin.data.prefs.Prefs;
+import com.tyanrv.loftcoin.screens.start.StartActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -15,9 +22,27 @@ public class WelcomeActivity extends AppCompatActivity {
         context.startActivity(starter);
     }
 
+    @BindView(R.id.pager)
+    ViewPager pager;
+
+    @BindView(R.id.start_btn)
+    Button startBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        ButterKnife.bind(this);
+
+
+        final Prefs prefs = ((App) getApplication()).getPrefs();
+
+        pager.setAdapter(new WelcomePagerAdapter(getSupportFragmentManager()));
+        startBtn.setOnClickListener(v -> {
+
+            prefs.setFirstLaunch(false);
+            StartActivity.start(WelcomeActivity.this);
+        });
     }
 }
