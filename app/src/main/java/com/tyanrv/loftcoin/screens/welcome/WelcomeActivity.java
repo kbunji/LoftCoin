8e9f9d6a1227f34853dbbd613538d1275a8555af
kztyanrv <tyanrv@gmail.com>
2019-03-24 16:04:01 +0600

    update StartActivity layout, fixed tasks

--- app/src/main/java/com/tyanrv/loftcoin/screens/welcome/WelcomeActivity.java
+++ app/src/main/java/com/tyanrv/loftcoin/screens/welcome/WelcomeActivity.java
@@ -25,6 +25,7 @@ public class WelcomeActivity extends AppCompatActivity {
 
     public static void startInNewTask(Context context) {
         Intent starter = new Intent(context, WelcomeActivity.class);
+        starter.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         context.startActivity(starter);
     }
 
