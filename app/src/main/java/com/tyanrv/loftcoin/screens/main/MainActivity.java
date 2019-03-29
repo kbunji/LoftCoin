7decc038d8c6afef6bc3a9052797905766234eae
kztyanrv <tyanrv@gmail.com>
2019-03-29 10:47:43 +0600

    update: MainActivity layout

--- app/src/main/java/com/tyanrv/loftcoin/screens/main/MainActivity.java
+++ app/src/main/java/com/tyanrv/loftcoin/screens/main/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.tyanrv.loftcoin.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
